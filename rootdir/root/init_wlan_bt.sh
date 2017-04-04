605b02a457dfabac05baa475be04c59ebb91c81e
Marius Gripsgard <mariogrip@ubuntu.com>
2017-04-04 20:36:40 +0200

    Add init_wlan_bt from
    This pack bt and wlan into one init script and makes sure bt does not
    get inited before wlan
    
    Change-Id: Id5a95dd9b1cf90bc5411d7a91d0bebb87fec67a2

---
+++ rootdir/root/init_wlan_bt.sh
@@ -0,0 +1,37 @@
+#!/system/bin/sh
+
+## The Ubports project
+## Wait for WCNSS service to setup WLAN device over QMI
+
+# Workaround for conn_init not copying the updated firmware
+rm /data/misc/wifi/WCNSS_qcom_cfg.ini
+rm /data/misc/wifi/WCNSS_qcom_wlan_nv.bin
+
+logwrapper /system/bin/conn_init
+
+echo 1 > /dev/wcnss_wlan
+
+enable_bt () {
+
+        if [[ `getprop ro.qualcomm.bt.hci_transport` != "smd" ]]; then
+            setprop ro.qualcomm.bt.hci_transport smd
+        fi
+
+        #initialize bt device
+        /system/bin/hci_qcomm_init -e
+        sleep 1 
+        echo 1 > /sys/module/hci_smd/parameters/hcismd_set
+
+
+}
+
+while true; do
+    sleep 2
+    if [ ! -f /sys/devices/fb000000.qcom,wcnss-wlan/net/wlan0/address ]; then
+        echo sta > /sys/module/wlan/parameters/fwpath
+    else
+        # enable bluetooth here since we have to wait for wlan to be initialized
+        enable_bt
+        break
+    fi
+done
